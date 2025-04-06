953bc79e7b400ecf527b14d14e7e07c27b761e60
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2025-04-06 12:04:44 +0200

    Small refactoring.

--- 14.validation/Ktt.Validation.Api/Services/ProvisionerService.cs
+++ 14.validation/Ktt.Validation.Api/Services/ProvisionerService.cs
@@ -3,9 +3,13 @@ using Ktt.Validation.Api.Services.Validation;
 
 namespace Ktt.Validation.Api.Services;
 
-public class ProvisionerService(IDataAnnotationsValidator validator)
+public class ProvisionerService(
+    IDataAnnotationsValidator validator
+)
 {
-    public void ProvisionApplication(ApplicationProvisioningRequest request)
+    public void ProvisionApplication(
+        ApplicationProvisioningRequest request
+    )
     {
         ArgumentNullException.ThrowIfNull(request, nameof(request));
         validator.ThrowIfInvalid(request, nameof(request));
