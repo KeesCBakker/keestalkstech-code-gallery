9d5c8b46c59cae048c7de2a8a2d50bdf9d25545d
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2025-04-08 14:28:12 +0200

    Refactor to extension methods.

--- 14.validation/Ktt.Validation.Api.Tests/Models/ComplexApplicationProvisioningTests.cs
+++ 14.validation/Ktt.Validation.Api.Tests/Models/ComplexApplicationProvisioningTests.cs
@@ -26,68 +26,67 @@ public class ComplexApplicationProvisioningTests
         };
 
         IList<ValidationResult> errors = [];
-        var result = false;
 
         //1. start with nothing -- required fields
-        result = validator.TryValidate(request, out errors);
-        errors.Should().Contain(e => A(e, "Cpu", "The Cpu field is required."));
-        errors.Should().Contain(e => A(e, "Environment", "The Environment field is required."));
-        errors.Should().Contain(e => A(e, "DockerHubRepo", "The DockerHubRepo field is required."));
-        errors.Should().Contain(e => A(e, "ImageTag", "The ImageTag field is required."));
-        errors.Should().Contain(e => A(e, "Ram", "The Ram field is required."));
+        validator.TryValidate(request, out errors);
+        errors.Should().Contain("Cpu", "The Cpu field is required.");
+        errors.Should().Contain("Environment", "The Environment field is required.");
+        errors.Should().Contain("DockerHubRepo", "The DockerHubRepo field is required.");
+        errors.Should().Contain("ImageTag", "The ImageTag field is required.");
+        errors.Should().Contain("Ram", "The Ram field is required.");
 
         // 2. cpu
 
         // 2.1 test invalid cpu
         request.Cpu = "blah";
-        result = validator.TryValidate(request, out errors);
-        errors.Should().Contain(e => A(e, "Cpu", "The field Cpu must match the regular expression '^\\d+m$'."));
+        validator.TryValidate(request, out errors);
+        errors.Should().Contain("Cpu", "The field Cpu must match the regular expression '^\\d+m$'.");
 
         // 2.2 test valid cpu
         request.Cpu = "100m";
-        result = validator.TryValidate(request, out errors);
-        errors.Should().NotContain(e => A(e, "Cpu"));
+        validator.TryValidate(request, out errors);
+        errors.Should().NotContain("Cpu");
 
         // 3. ram
 
         // 3.1 test invalid ram
         request.Ram = "blah";
-        result = validator.TryValidate(request, out errors);
-        errors.Should().Contain(e => A(e, "Ram", "The field Ram must match the regular expression '^\\d+Mi$'."));
+        validator.TryValidate(request, out errors);
+        errors.Should().Contain("Ram", "The field Ram must match the regular expression '^\\d+Mi$'.");
 
         // 3.2 test valid ram
         request.Ram = "100Mi";
-        result = validator.TryValidate(request, out errors);
-        errors.Should().NotContain(e => A(e, "Ram"));
+        validator.TryValidate(request, out errors);
+        errors.Should().NotContain("Ram");
 
         // 4. image tag
         request.ImageTag = "12-abcefe";
-        result = validator.TryValidate(request, out errors);
-        errors.Should().NotContain(e => A(e, "Ram"));
+        validator.TryValidate(request, out errors);
+        errors.Should().NotContain("Ram");
 
         // 5. environment
         request.Environment = "blah";
-        result = validator.TryValidate(request, out errors);
-        errors.Should().NotContain(e => A(e, "Environment"));
+        validator.TryValidate(request, out errors);
+        errors.Should().NotContain("Environment");
 
         // 6. dockerhub repo
         request.DockerHubRepo = "blah";
-        result = validator.TryValidate(request, out errors);
-        errors.Should().NotContain(e => A(e, "DockerHubRepo", "The DockerHubRepo field is required."));
+        validator.TryValidate(request, out errors);
+        errors.Should().NotContain("DockerHubRepo", "The DockerHubRepo field is required.");
 
         // 7. we should still have errors
-        errors.Should().Contain(e => A(e, "DockerHubRepo", "DockerHub repo must exist."));
-        errors.Should().Contain(e => A(e, "Environment", "Environment must exist."));
+        errors.Should().Contain("DockerHubRepo", "DockerHub repo must exist.");
+        errors.Should().Contain("Environment", "Environment must exist.");
 
         // 8. valid environment
         request.Environment = "server-one";
-        result = validator.TryValidate(request, out errors);
-        errors.Should().NotContain(e => A(e, "Environment"));
+        validator.TryValidate(request, out errors);
+        errors.Should().NotContain("Environment");
 
         // 9. valid dockerhub repo
         request.DockerHubRepo = "repo-one";
-        result = validator.TryValidate(request, out errors);
-        errors.Should().NotContain(e => A(e, "DockerHubRepo"));
+        validator.TryValidate(request, out errors);
+        errors.Should().NotContain("DockerHubRepo");
     }
 
     [Theory]
@@ -109,17 +108,16 @@ public class ComplexApplicationProvisioningTests
         };
 
         IList<ValidationResult> errors = [];
-        var result = false;
 
         // 1. schedule may not be provided
         request.Schedule = "blah";
-        result = validator.TryValidate(request, out errors);
-        errors.Should().Contain(e => A(e, "Schedule", "'Schedule' must be empty."));
+        validator.TryValidate(request, out errors);
+        errors.Should().Contain("Schedule", "'Schedule' must be empty.");
 
         // 2. validate schedule empty
         request.Schedule = string.Empty;
-        result = validator.TryValidate(request, out errors);
-        errors.Should().NotContain(e => A(e, "Schedule"));
+        validator.TryValidate(request, out errors);
+        errors.Should().NotContain("Schedule");
     }
 
     [Theory]
@@ -141,22 +139,21 @@ public class ComplexApplicationProvisioningTests
         };
 
         IList<ValidationResult> errors = [];
-        var result = false;
 
         // 1. schedule must be provider
         request.Schedule = string.Empty;
-        result = validator.TryValidate(request, out errors);
-        errors.Should().Contain(e => A(e, "Schedule", "'Schedule' must not be empty."));
+        validator.TryValidate(request, out errors);
+        errors.Should().Contain("Schedule", "'Schedule' must not be empty.");
 
         // 2. invalid schedule
         request.Schedule = "maandag de 14e";
-        result = validator.TryValidate(request, out errors);
-        errors.Should().Contain(e => A(e, "Schedule", "Schedule must be a valid cron expression."));
+        validator.TryValidate(request, out errors);
+        errors.Should().Contain("Schedule", "Schedule must be a valid cron expression.");
 
         // 2. value schedule
         request.Schedule = "5 4 * * *";
-        result = validator.TryValidate(request, out errors);
-        errors.Should().NotContain(e => A(e, "Schedule"));
+        validator.TryValidate(request, out errors);
+        errors.Should().NotContain("Schedule");
     }
 
     [Theory]
@@ -178,41 +175,40 @@ public class ComplexApplicationProvisioningTests
         };
 
         IList<ValidationResult> errors = [];
-        var result = false;
 
         // 1. Some fields must be provided
         request.Command = string.Empty;
         request.Postfix = string.Empty;
-        result = validator.TryValidate(request, out errors);
-        errors.Should().Contain(e => A(e, "Command", "'Command' must not be empty."));
-        errors.Should().Contain(e => A(e, "Postfix", "'Postfix' must not be empty."));
+        validator.TryValidate(request, out errors);
+        errors.Should().Contain("Command", "'Command' must not be empty.");
+        errors.Should().Contain("Postfix", "'Postfix' must not be empty.");
 
         // 2. Scripts without tini should not be allowed
         request.Command = "/app/start.sh service-a";
-        result = validator.TryValidate(request, out errors);
-        errors.Should().Contain(e => A(e, "Command", "Script files (.sh) may only be executed when tini is used."));
+        validator.TryValidate(request, out errors);
+        errors.Should().Contain("Command", "Script files (.sh) may only be executed when tini is used.");
 
         // 3. Script with tini should be allowed
         request.Command = "tini /app/start.sh service-a";
-        result = validator.TryValidate(request, out errors);
-        errors.Should().NotContain(e => A(e, "Command"));
+        validator.TryValidate(request, out errors);
+        errors.Should().NotContain("Command");
 
         // 4. Validate invalid postfix
 
         // 4.1 Postfix invalid due to casing
         request.Postfix = "I'm a Service";
-        result = validator.TryValidate(request, out errors);
-        errors.Should().Contain(e => A(e, "Postfix", "The value must be lower-kebab-case."));
+        validator.TryValidate(request, out errors);
+        errors.Should().Contain("Postfix", "The value must be lower-kebab-case.");
 
         // 4.2 Postfix invalid due to forbidden words
         request.Postfix = "cron-site-service";
-        result = validator.TryValidate(request, out errors);
-        errors.Should().Contain(e => A(e, "Postfix", "The value may not contain the words cron, site or service."));
+        validator.TryValidate(request, out errors);
+        errors.Should().Contain("Postfix", "The value may not contain the words cron, site or service.");
 
         // 4.3 Postfix valid
         request.Postfix = "pinger";
-        result = validator.TryValidate(request, out errors);
-        errors.Should().NotContain(e => A(e, "Postfix"));
+        validator.TryValidate(request, out errors);
+        errors.Should().NotContain("Postfix");
     }
 
     [Theory]
@@ -234,37 +230,26 @@ public class ComplexApplicationProvisioningTests
         };
 
         IList<ValidationResult> errors = [];
-        var result = false;
 
         // 1. Some fields must be empty
         request.Command = "dotnet run /app/kaas.dll";
         request.Postfix = "kaas";
-        result = validator.TryValidate(request, out errors);
-        errors.Should().Contain(e => A(e, "Command", "'Command' must be empty."));
-        errors.Should().Contain(e => A(e, "Postfix", "'Postfix' must be empty."));
-
+        validator.TryValidate(request, out errors);
+        errors.Should().Contain("Command", "'Command' must be empty.");
+        errors.Should().Contain("Postfix", "'Postfix' must be empty.");
+        
         // 2. Command is not allowed
         request.Command = string.Empty;
-        result = validator.TryValidate(request, out errors);
-        errors.Should().NotContain(e => A(e, "Command"));
+        validator.TryValidate(request, out errors);
+        errors.Should().NotContain("Command");
 
         // 3. Postfix is not allowed
         request.Postfix = string.Empty;
-        result = validator.TryValidate(request, out errors);
-        errors.Should().NotContain(e => A(e, "Postfix"));
+        validator.TryValidate(request, out errors);
+        errors.Should().NotContain("Postfix");
     }
 
-    public bool A(ValidationResult e, string memberName, string msg)
-    {
-        return e.ErrorMessage == msg && e.MemberNames.Any(m => m == memberName);
-    }
-
-    public bool A(ValidationResult e, string memberName)
-    {
-        return e.MemberNames.Any(m => m == memberName);
-    }
-
-    public IServiceProvider GetServiceProvider()
+    public static IServiceProvider GetServiceProvider()
     {
         string[] environments = ["server-one"];
         string[] repos = ["repo-one"];
---
+++ 14.validation/Ktt.Validation.Api.Tests/Models/ValidationResultAssertionsExtensions.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using FluentAssertions.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ktt.Validation.Api.Tests.Models;
+
+public static class ValidationResultAssertionsExtensions
+{
+    public static AndConstraint<GenericCollectionAssertions<ValidationResult>> Contain(
+        this GenericCollectionAssertions<ValidationResult> assertions,
+        string memberName,
+        string? errorMessage = null)
+    {
+        return assertions.Contain(e =>
+            (errorMessage == null || e.ErrorMessage == errorMessage) &&
+            e.MemberNames.Contains(memberName)
+        );
+    }
+
+    public static AndConstraint<GenericCollectionAssertions<ValidationResult>> NotContain(
+        this GenericCollectionAssertions<ValidationResult> assertions,
+        string memberName,
+        string? errorMessage = null)
+    {
+        return assertions.NotContain(e =>
+            (errorMessage == null || e.ErrorMessage == errorMessage) &&
+            e.MemberNames.Contains(memberName)
+        );
+    }
+}
