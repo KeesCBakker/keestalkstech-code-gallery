c7f1c5a1b32d144ade84a3f98bb12337bccfd52b
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2026-07-20 08:44:20 +0200

    refactor: split ManifestResourceNotFoundException into own file

--- 06.handlebars/Ktt.JsonHandlebars/JsonTemplateGeneratorExtensions.cs
+++ 06.handlebars/Ktt.JsonHandlebars/JsonTemplateGeneratorExtensions.cs
@@ -33,7 +33,3 @@ public static class JsonTemplateGeneratorExtensions
         return reader.ReadToEnd();
     }
 }
-
-public class ManifestResourceNotFoundException(string name, Assembly assembly) : Exception($"Manifest resource with name '{name}' in assembly '{assembly.FullName}' not found.")
-{
-}
---
+++ 06.handlebars/Ktt.JsonHandlebars/ManifestResourceNotFoundException.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+namespace Ktt.JsonHandlebars;
+
+public class ManifestResourceNotFoundException(string name, Assembly assembly) : Exception($"Manifest resource with name '{name}' in assembly '{assembly.FullName}' not found.")
+{
+}
