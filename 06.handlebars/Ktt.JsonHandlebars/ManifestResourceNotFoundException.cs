155af53c3c89811ed8a4705355513f60080fb866
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2026-07-20 08:56:26 +0200

    style: format ManifestResourceNotFoundException base call

--- 06.handlebars/Ktt.JsonHandlebars/ManifestResourceNotFoundException.cs
+++ 06.handlebars/Ktt.JsonHandlebars/ManifestResourceNotFoundException.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 
 namespace Ktt.JsonHandlebars;
 
-public class ManifestResourceNotFoundException(string name, Assembly assembly) : Exception($"Manifest resource with name '{name}' in assembly '{assembly.FullName}' not found.")
+public class ManifestResourceNotFoundException(string name, Assembly assembly)
+    : Exception($"Manifest resource with name '{name}' in assembly '{assembly.FullName}' not found.")
 {
 }
